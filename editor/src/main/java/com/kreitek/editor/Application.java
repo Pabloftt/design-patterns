1d46b15031656d29bd6a9975386c300d60d8104b
Pablo Fornetti <pablofornetti@gmail.com>
2022-04-07 23:15:06 +0200

    final commit

--- editor/src/main/java/com/kreitek/editor/Application.java
+++ editor/src/main/java/com/kreitek/editor/Application.java
@@ -4,8 +4,14 @@ public class Application {
 
     public static void main(String[] args) {
         EditorFactory editorFactory = new EditorFactory();
-        Editor editor = editorFactory.getEditor();
-        editor.run();
-    }
+        Editor editor = editorFactory.getEditor("TEXT");
+        Editor editor1 = editorFactory.getEditor("JSON");
 
+        if(args[0].equalsIgnoreCase("text")) {
+            editor.run();
+        }
+        else if(args[0].equalsIgnoreCase("json"));{
+            editor1.run();
+        }
+    }
 }
--- editor/src/main/java/com/kreitek/editor/EditorFactory.java
+++ editor/src/main/java/com/kreitek/editor/EditorFactory.java
@@ -1,7 +1,18 @@
 package com.kreitek.editor;
 
 public class EditorFactory {
-    public Editor getEditor() {
-        return new ConsoleEditor();
+
+
+    public Editor getEditor(String consoleType) {
+        if(consoleType == null){
+            return null;
+        }
+        else if(consoleType.equalsIgnoreCase("JSON")){
+            return new JsonEditor();
+        }
+        else if(consoleType.equalsIgnoreCase("TEXT")){
+            return new ConsoleEditor();
+        }
+        return null;
     }
 }
